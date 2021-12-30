a2ab75916d450d50f7a21cdf472217d4cede0afa
Adroaldo <hygino@msn.com>
2021-12-30 11:06:27 -0300

    Testando com uma iteração

--- src/model/Rede.java
+++ src/model/Rede.java
@@ -2,7 +2,7 @@ package model;
 
 public class Rede {
 
-    private Double[][] x;
+    private Double x[][];
     private Double d[];
     private Double w[];
     private Double n;
@@ -20,7 +20,7 @@ public class Rede {
         this.teta = teta;
         this.xTeta = xTeta;
     }
-   
+
     public Double[][] getX() {
         return x;
     }
@@ -80,7 +80,59 @@ public class Rede {
         }
     }
 
+    public void mostrarD() {
+        System.out.printf("O vetor D tem %d elementos\n", d.length);
+        for (int i = 0; i < d.length; i++) {
+            System.out.printf("%.4f\n", d[i]);
+        }
+    }
+
+    public void mostrarW() {
+        for (int i = 0; i < w.length; i++) {
+            System.out.printf("w[%d] = %.4f\t", (i + 1), w[i]);
+        }
+        System.out.println();
+    }
+
     public static double funcaoAtivacao(double x) {
-        return 1 / (1 + Math.exp(-x)); 
+        return 1 / (1 + Math.exp(-x));
+    }
+
+    public void resolver(int numeroIteracoes) {
+        int iteracoes = 0;
+
+        while (iteracoes < numeroIteracoes) {
+            iteracoes++;
+            double valorIteracao = 0.0;
+            System.out.printf("Iteração #%d\n", iteracoes);
+            for (int i = 0; i < d.length; i++) {
+                for (int j = 0; j < w.length; j++) {
+                    valorIteracao += w[j] * x[i][j];
+                }
+                valorIteracao +=  teta;//+xteta
+                System.out.printf("i_A[%d] = %.4f\n", (i + 1), valorIteracao);
+                double funAtivacao = funcaoAtivacao(valorIteracao);
+                System.out.printf("Função de ativação A[%d] = %.4f\n", (i + 1), funAtivacao);
+                double nTaxa = d[i] - funAtivacao;
+                System.out.printf("Nova taxa = %.4f\n", nTaxa);
+
+                double[] varW = new double[w.length];
+
+                for (int j = 0; j < w.length; j++) {
+                    // System.out.printf("Anterior w[%d] = %.4f\n", (j + 1), w[j]);
+
+                    varW[j] = n * d[j] * nTaxa;
+
+                    System.out.printf("varW[%d] = %.4f\n", (j + 1), varW[j]);
+                    w[j] += varW[j];
+                    System.out.printf("Novo w[%d] = %.4f\n", (j + 1), w[j]);
+                }
+                double varTeta = n * xTeta * nTaxa;
+                teta += varTeta;
+                System.out.printf("Teta = %f\n", teta);
+            }
+
+        }
+
     }
 }
