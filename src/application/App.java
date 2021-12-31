e68a74f3b99932d6d1824d7948a0e4647fcb585e
Adroaldo <hygino@msn.com>
2021-12-31 08:13:13 -0300

    Testando exemplos

---
+++ src/application/App.java
@@ -0,0 +1,97 @@
+package application;
+
+import java.util.Locale;
+
+import model.Rede;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+		Locale.setDefault(Locale.US);
+
+		System.out.println("Redes Neurais Artificiais pelo método Back Propagation");
+		exemplo01();
+		// exemplo02();
+		//exemplo03();
+		//exemplo04();
+
+	}
+
+	public static void exemplo01() {
+		Double[][] A = { { 1.0, 1.0 }, { 2.0, 2.0 }, { 1.0, 3.0 }, { 2.5, 1.0 }, { 3.0, 3.0 } };
+		Double[] D = { 1.0, 1.0, 1.0, 0.0, 0.0 };
+		Double[] W = { 0.0, 0.0 };
+		Double n = 0.6;
+		Double teta = 0.0, xTeta = 1.0;
+
+		Rede rede = new Rede(A, D, W, n, teta, xTeta);
+
+		rede.mostrarX();
+		linha();
+		rede.mostrarD();
+		linha();
+		rede.mostrarW();
+		System.out.printf("Teta = %.4f\txTeta = %.4f\n", teta, xTeta);
+		linha();
+		rede.resolver(1);
+	}
+
+	public static void exemplo02() {
+		Double[][] A = { { 1.0, 1.5 }, { 2.0, 0.5 } };
+		Double[] D = { 0.0, 1.0 };
+		Double[] W = { 0.7, 1.0 };
+		Double n = 0.6;
+		Double teta = 0.0, xTeta = 1.0;
+
+		Rede rede = new Rede(A, D, W, n, teta, xTeta);
+
+		rede.mostrarX();
+		linha();
+		rede.mostrarD();
+		linha();
+		rede.mostrarW();
+		System.out.printf("Teta = %.4f\txTeta = %.4f\n", teta, xTeta);
+		linha();
+		rede.resolver(1);
+
+	}
+
+	public static void exemplo03() {
+		Double[][] A = { { 2.5, 0.0 }, { 1.0, 1.5 }, { 0.0, 1.0 } };
+		Double[] D = { 0.0, 1.0, 1.0 };
+		Double[] W = { 0.5, 0.5 };
+		Double n = 0.9, teta = 0.5, xTeta = 1.0;
+
+		Rede rede = new Rede(A, D, W, n, teta, xTeta);
+
+		rede.mostrarX();
+		linha();
+		rede.mostrarD();
+		linha();
+		rede.mostrarW();
+		System.out.printf("Teta = %.4f\txTeta = %.4f\n", teta, xTeta);
+		linha();
+		rede.resolver(1);
+	}
+	
+	public static void exemplo04() {
+		Double[][] A = { { 1.5, 2.0 }, { 1.0, 1.0 }, { 0.0, 1.0 } };
+		Double[] D = { 1.0, 1.0, 1.0 };
+		Double[] W = { 0.8, 0.8 };
+		Double n = 0.5, teta = 0.4, xTeta = 1.0;
+
+		Rede rede = new Rede(A, D, W, n, teta, xTeta);
+
+		rede.mostrarX();
+		linha();
+		rede.mostrarD();
+		linha();
+		rede.mostrarW();
+		System.out.printf("Teta = %.4f\txTeta = %.4f\n", teta, xTeta);
+		linha();
+		rede.resolver(1);
+	}
+
+	public static void linha() {
+		System.out.println("--------------------------------------------\n");
+	}
+}
--- src/model/Rede.java
+++ src/model/Rede.java
@@ -2,137 +2,140 @@ package model;
 
 public class Rede {
 
-    private Double x[][];
-    private Double d[];
-    private Double w[];
-    private Double n;
-    private Double teta;
-    private Double xTeta;
+	private Double x[][];
+	private Double d[];
+	private Double w[];
+	private Double n;
+	private Double teta;
+	private Double xTeta;
 
-    public Rede() {
-    }
+	public Rede() {
+	}
 
-    public Rede(Double[][] x, Double[] d, Double[] w, Double n, Double teta, Double xTeta) {
-        this.x = x;
-        this.d = d;
-        this.w = w;
-        this.n = n;
-        this.teta = teta;
-        this.xTeta = xTeta;
-    }
+	public Rede(Double[][] x, Double[] d, Double[] w, Double n, Double teta, Double xTeta) {
+		this.x = x;
+		this.d = d;
+		this.w = w;
+		this.n = n;
+		this.teta = teta;
+		this.xTeta = xTeta;
+	}
 
-    public Double[][] getX() {
-        return x;
-    }
+	public Double[][] getX() {
+		return x;
+	}
 
-    public void setX(Double[][] x) {
-        this.x = x;
-    }
+	public void setX(Double[][] x) {
+		this.x = x;
+	}
 
-    public Double[] getD() {
-        return d;
-    }
+	public Double[] getD() {
+		return d;
+	}
 
-    public void setD(Double[] d) {
-        this.d = d;
-    }
+	public void setD(Double[] d) {
+		this.d = d;
+	}
 
-    public Double[] getW() {
-        return w;
-    }
+	public Double[] getW() {
+		return w;
+	}
 
-    public void setW(Double[] w) {
-        this.w = w;
-    }
+	public void setW(Double[] w) {
+		this.w = w;
+	}
 
-    public Double getN() {
-        return n;
-    }
+	public Double getN() {
+		return n;
+	}
 
-    public void setN(Double n) {
-        this.n = n;
-    }
+	public void setN(Double n) {
+		this.n = n;
+	}
 
-    public Double getTeta() {
-        return teta;
-    }
+	public Double getTeta() {
+		return teta;
+	}
 
-    public void setTeta(Double teta) {
-        this.teta = teta;
-    }
+	public void setTeta(Double teta) {
+		this.teta = teta;
+	}
 
-    public Double getxTeta() {
-        return xTeta;
-    }
+	public Double getxTeta() {
+		return xTeta;
+	}
 
-    public void setxTeta(Double xTeta) {
-        this.xTeta = xTeta;
-    }
+	public void setxTeta(Double xTeta) {
+		this.xTeta = xTeta;
+	}
 
-    public void mostrarX() {
-        int linhas = x.length;
-        int colunas = x[0].length;
-        System.out.printf("A matriz X tem %d linhas e %d colunas\n", linhas, colunas);
-        for (int i = 0; i < linhas; i++) {
-            for (int j = 0; j < colunas; j++)
-                System.out.printf("%.4f\t", x[i][j]);
-            System.out.println();
-        }
-    }
+	public void mostrarX() {
+		int linhas = x.length;
+		int colunas = x[0].length;
+		System.out.printf("A matriz X tem %d linhas e %d colunas\n", linhas, colunas);
+		for (int i = 0; i < linhas; i++) {
+			for (int j = 0; j < colunas; j++)
+				System.out.printf("%.9f\t", x[i][j]);
+			System.out.println();
+		}
+	}
 
-    public void mostrarD() {
-        System.out.printf("O vetor D tem %d elementos\n", d.length);
-        for (int i = 0; i < d.length; i++) {
-            System.out.printf("%.4f\n", d[i]);
-        }
-    }
+	public void mostrarD() {
+		System.out.printf("O vetor D tem %d elementos\n", d.length);
+		for (int i = 0; i < d.length; i++) {
+			System.out.printf("%.9f\n", d[i]);
+		}
+	}
 
-    public void mostrarW() {
-        for (int i = 0; i < w.length; i++) {
-            System.out.printf("w[%d] = %.4f\t", (i + 1), w[i]);
-        }
-        System.out.println();
-    }
+	public void mostrarW() {
+		for (int i = 0; i < w.length; i++) {
+			System.out.printf("w[%d] = %.9f\t", (i + 1), w[i]);
+		}
+		System.out.println();
+	}
 
-    public static double funcaoAtivacao(double x) {
-        return 1 / (1 + Math.exp(-x));
-    }
+	public static double funcaoAtivacao(double x) {
+		return 1 / (1 + Math.exp(-x));
+	}
 
-    public void resolver(int numeroIteracoes) {
-        int iteracoes = 0;
+	public void resolver(int numeroIteracoes) {
+		int iteracoes = 0;
 
-        while (iteracoes < numeroIteracoes) {
-            iteracoes++;
-            double valorIteracao = 0.0;
-            System.out.printf("IteraÃ§Ã£o #%d\n", iteracoes);
-            for (int i = 0; i < d.length; i++) {
-                for (int j = 0; j < w.length; j++) {
-                    valorIteracao += w[j] * x[i][j];
-                }
-                valorIteracao +=  teta;//+xteta
-                System.out.printf("i_A[%d] = %.4f\n", (i + 1), valorIteracao);
-                double funAtivacao = funcaoAtivacao(valorIteracao);
-                System.out.printf("FunÃ§Ã£o de ativaÃ§Ã£o A[%d] = %.4f\n", (i + 1), funAtivacao);
-                double nTaxa = d[i] - funAtivacao;
-                System.out.printf("Nova taxa = %.4f\n", nTaxa);
+		while (iteracoes < numeroIteracoes) {
+			iteracoes++;
 
-                double[] varW = new double[w.length];
+			// double valorIteracao = teta;// xteta
+			System.out.printf("Iteração #%d\n", iteracoes);
+			for (int i = 0; i < d.length; i++) {
+				double valorIteracao = teta;// xteta
 
-                for (int j = 0; j < w.length; j++) {
-                    // System.out.printf("Anterior w[%d] = %.4f\n", (j + 1), w[j]);
+				for (int u = 0; u < w.length; u++) {
+					valorIteracao += w[u] * x[i][u];// w[0]
+					System.out.printf("X[%d] = %.9f\n", (u + 1), x[i][u]);
+				}
+				// passagem por valorIteracao += w[0] * x[i][0] + teta;
+				System.out.printf("i_A[%d] = %.9f\n", (i + 1), valorIteracao);
+				double funAtivacao = funcaoAtivacao(valorIteracao);
+				System.out.printf("Função de ativação A[%d] = %.9f\n", (i + 1), funAtivacao);
+				double nTaxa = d[i] - funAtivacao;
+				double[] varW = new double[w.length];
 
-                    varW[j] = n * d[j] * nTaxa;
+				System.out.printf("Nova taxa = %.9f\n", nTaxa);
 
-                    System.out.printf("varW[%d] = %.4f\n", (j + 1), varW[j]);
-                    w[j] += varW[j];
-                    System.out.printf("Novo w[%d] = %.4f\n", (j + 1), w[j]);
-                }
-                double varTeta = n * xTeta * nTaxa;
-                teta += varTeta;
-                System.out.printf("Teta = %f\n", teta);
-            }
+				for (int j = 0; j < w.length; j++) {
+					varW[j] = nTaxa * x[i][j] * n;
+					w[j] += varW[j];
+					System.out.printf("varW[%d] = %.9f\n", (j + 1), varW[j]);
+					System.out.printf("Novo w[%d] = %.9f\n", (j + 1), w[j]);
+				}
+				double deltaTeta = nTaxa * xTeta * n;
+				System.out.printf("VarTeta = %.9f\n", deltaTeta);
+				teta += deltaTeta;
+				System.out.printf("Teta = %.9f\n\n", teta);
 
-        }
+			}
 
-    }
+		}
+
+	}
 }
